032ddeb7df58bbc6e52ea21a71978e9cac91d426
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-09-21 16:11:43 -0600

    Whitenoise 4.0 fixes

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/common.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/common.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,7 +53,6 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = '{{ cookiecutter.repo_name }}.urls'
