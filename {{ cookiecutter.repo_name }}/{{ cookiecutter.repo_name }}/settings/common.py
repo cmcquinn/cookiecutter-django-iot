c4c7cd0a797360f977bc74740680fee42f69fdb6
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-09-21 16:02:21 -0600

    Change MIDDLEWARE_CLASSES to MIDDLEWARE

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/common.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/common.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     '{{ cookiecutter.repo_name }}.apps.interactions',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
