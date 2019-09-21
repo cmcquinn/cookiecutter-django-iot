50660d0eeb06dff53402ce5021ddaedce94e5abb
Cameron McQuinn <cameron.mcquinn@gmail.com>
2019-09-21 15:52:13 -0600

    More django 2 fixes

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home')
 ]
